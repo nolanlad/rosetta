3f67a6b49224378b7bfbc504ce394acd0c975522
nolanlad <nolanrmccarthy@gmail.com>
2018-04-20 10:42:11 -0700

    idk

--- test_std.cpp
+++ test_std.cpp
@@ -10,6 +10,13 @@ int main()
    vector<double> xd;
    converter(xd,x);
    cout << xd.at(0) << endl;
+   vector<vector<double>> in(2);
+   //cout << in[0][0] << endl;
+   in.at(0) = {2.,2.,2.};
+   in.at(1) = {2.,2.,2.};
+   vector<double> in2 = {3., 2., 1.};
+   in.at(0) = in2;
+   cout << in[0][0] << endl;
    return 1;
 }
 
